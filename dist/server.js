3d297611d255dd19f16f548ec6c690e0983cc2a5
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2026-05-04 20:45:43 -0300

    fix: only load dotenv in development, not production

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const fastify_1 = __importDefault(require("fastify"));
 const prisma_1 = __importDefault(require("./plugins/prisma"));
 const jwt_1 = __importDefault(require("./plugins/jwt"));
